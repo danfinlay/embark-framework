1e72d5b2d3a04a178bfb2a9994c314dbd3f2dfe0
Dan Finlay <danfinlay@apple.com>
2015-12-28 23:24:44 -0800

    Convert some of chain_manager to async
    Converted the following methods to async, promise-returning methods:
    
     - loadConfigFile
     - init
    
    Also:
    
     - Commented out broken test/test.js file.
     - Improved some `else` formatting.
     - Changed a strict assertion to what it needed to expect (which was
         one space off).

--- test/chain_manager.js
+++ test/chain_manager.js
@@ -12,29 +12,42 @@ describe('embark.chain_manager', function() {
   var chainFile = './test/support/chain_manager.json';
   fs.writeFileSync(chainFile, '{}');
 
-  var chainManager = (new ChainManager()).loadConfigFile(chainFile);
   var blockchainConfig = (new Config.Blockchain()).loadConfigFile('test/support/blockchain.yml').config('development');
 
   describe('#init', function() {
-    chainManager.init('development', blockchainConfig, web3);
-
     it('should initialize chain', function() {
-      var chain = chainManager.chainManagerConfig['0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5']
-      assert.equal(chain != undefined, true);
+
+      (new ChainManager()).loadConfigFile(chainFile)
+      .then(function(chainManager) {
+
+        chainManager.init('development', blockchainConfig, web3)
+        .then(function() {
+          var chain = chainManager.chainManagerConfig['0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5']
+          assert.notEqual(chain, undefined);
+        })
+        .catch(function(error) {
+          assert.ifError(error);
+        });
+      });
     });
   });
 
   describe('#addContract', function() {
 
     it('should register a contract in the chain', function() {
-      chainManager.addContract("Foo", "123456", [], "0x123");
 
-      console.log(chainManager.chainManagerConfig);
-      var chain = chainManager.chainManagerConfig['0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5']
-      var contract = chain.contracts["d5d91a8825c5c253dff531ddda2354c4014f5699b7bcbea70207cfdcb37b6c8b"]
+      (new ChainManager()).loadConfigFile(chainFile)
+      .then(function(chainManager) {
 
-      assert.equal(contract.name, "Foo");
-      assert.equal(contract.address, "0x123");
+        chainManager.addContract("Foo", "123456", [], "0x123");
+
+        console.log(chainManager.chainManagerConfig);
+        var chain = chainManager.chainManagerConfig['0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5']
+        var contract = chain.contracts["d5d91a8825c5c253dff531ddda2354c4014f5699b7bcbea70207cfdcb37b6c8b"]
+
+        assert.equal(contract.name, "Foo");
+        assert.equal(contract.address, "0x123");
+      });
     });
 
   });
@@ -42,24 +55,31 @@ describe('embark.chain_manager', function() {
   describe('#getContract', function() {
 
     it('should a contract in the chain', function() {
-      var contract = chainManager.getContract("Foo", "123456", []);
 
-      assert.equal(contract.name, "Foo");
-      assert.equal(contract.address, "0x123");
+      (new ChainManager()).loadConfigFile(chainFile)
+      .then(function(chainManager) {
+
+        var contract = chainManager.getContract("Foo", "123456", []);
+
+        assert.equal(contract.name, "Foo");
+        assert.equal(contract.address, "0x123");
+      });
     });
-
   });
 
   describe('#save', function() {
 
     it('should save changes in the chain', function() {
-      chainManager.save();
 
-      var chainFile = './test/support/chain_manager.json';
-      var content = fs.readFileSync(chainFile).toString();
-      assert.equal(content, '{"0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5":{"contracts":{"d5d91a8825c5c253dff531ddda2354c4014f5699b7bcbea70207cfdcb37b6c8b\":{"name":"Foo","address":"0x123"}}}}');
+      (new ChainManager()).loadConfigFile(chainFile)
+      .then(function(chainManager) {
+
+        chainManager.save();
+
+        var chainFile = './test/support/chain_manager.json';
+        var content = fs.readFileSync(chainFile).toString();
+        assert.equal(content, '{"0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5":{"contracts":{"d5d91a8825c5c253dff531ddda2354c4014f5699b7bcbea70207cfdcb37b6c8b\":{"name":"Foo","address":"0x123"}}}}');
+      });
     });
-
   });
-
 });
--- test/deploy.js
+++ test/deploy.js
@@ -14,11 +14,13 @@ setDeployConfig = function(config) {
   var blockchainConfig = _blockchainConfig.config("development");
   var compiler = new Compiler(_blockchainConfig);
   var contractsConfig = new Config.Contracts(blockchainConfig, compiler);
-  var chainManager = (new ChainManager()).loadConfigFile('./test/support/chain_manager.json');
-  contractsConfig.loadConfigFile(config.contracts);
-  contractsConfig.init(config.files, 'development');
-  compiler.init('development');
-  return new Deploy('development', config.files, blockchainConfig, contractsConfig, chainManager);
+  return (new ChainManager()).loadConfigFile('./test/support/chain_manager.json')
+  .then(function(chainManager) {
+    contractsConfig.loadConfigFile(config.contracts);
+    contractsConfig.init(config.files, 'development');
+    compiler.init('development');
+    return new Deploy('development', config.files, blockchainConfig, contractsConfig, chainManager);
+  });
 }
 
 describe('embark.deploy', function() {
@@ -32,43 +34,47 @@ describe('embark.deploy', function() {
     ];
 
     describe('#deploy_contracts', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/arguments.yml'
-      });
-      deploy.deploy_contracts("development");
-
       it("should deploy contracts", function() {
-        var all_contracts = ['Wallet', 'SimpleStorage', 'AnotherStorage', 'Wallets'];
-        for(var i=0; i < all_contracts.length; i++) {
-          var className = all_contracts[i];
 
-          assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
-        }
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/arguments.yml'
+        })
+        .then(function(deploy) {
+          deploy.deploy_contracts("development");
+
+          var all_contracts = ['Wallet', 'SimpleStorage', 'AnotherStorage', 'Wallets'];
+          for(var i=0; i < all_contracts.length; i++) {
+            var className = all_contracts[i];
+
+            assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
+          }
+        });
       });
-
     });
 
     describe('#generate_abi_file', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/arguments.yml'
-      });
-      deploy.deployedContracts = {
-        "SimpleStorage": "0x123",
-        "AnotherStorage": "0x234"
-      }
-      deploy.contractDB = {
-        "SimpleStorage":  {compiled: {info: {abiDefinition: 123}}},
-        "AnotherStorage": {compiled: {info: {abiDefinition: 234}}}
-      }
-
       it("should deploy contracts", function() {
-        var result = deploy.generate_abi_file();
 
-        assert.strictEqual(result, "web3.setProvider(new web3.providers.HttpProvider('http://localhost:8101'));web3.eth.defaultAccount = web3.eth.accounts[0];SimpleStorageAbi = 123;SimpleStorageContract = web3.eth.contract(SimpleStorageAbi);SimpleStorage = SimpleStorageContract.at('0x123');AnotherStorageAbi = 234;AnotherStorageContract = web3.eth.contract(AnotherStorageAbi);AnotherStorage = AnotherStorageContract.at('0x234');");
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/arguments.yml'
+        })
+        .then(function(deploy) {
+          deploy.deployedContracts = {
+            "SimpleStorage": "0x123",
+            "AnotherStorage": "0x234"
+          }
+          deploy.contractDB = {
+            "SimpleStorage":  {compiled: {info: {abiDefinition: 123}}},
+            "AnotherStorage": {compiled: {info: {abiDefinition: 234}}}
+          }
+
+          var result = deploy.generate_abi_file();
+          assert.strictEqual(result, "web3.setProvider(new web3.providers.HttpProvider('http://localhost:8101'));web3.eth.defaultAccount = web3.eth.accounts[0];SimpleStorageAbi = 123;SimpleStorageContract = web3.eth.contract(SimpleStorageAbi);SimpleStorage = SimpleStorageContract.at('0x123');AnotherStorageAbi = 234;AnotherStorageContract = web3.eth.contract(AnotherStorageAbi);AnotherStorage = AnotherStorageContract.at('0x234');");
+        });
       });
     });
   });
@@ -80,24 +86,25 @@ describe('embark.deploy', function() {
     ];
 
     describe('#deploy_contracts', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/arguments2.yml'
-      });
-      deploy.deploy_contracts("development");
 
       it("should deploy contracts", function() {
-        var all_contracts = ['token', 'Crowdsale'];
-        for(var i=0; i < all_contracts.length; i++) {
-          var className = all_contracts[i];
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/arguments2.yml'
+        })
+        .then(function(deploy) {
+          deploy.deploy_contracts("development");
 
-          assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
-        }
+          var all_contracts = ['token', 'Crowdsale'];
+          for(var i=0; i < all_contracts.length; i++) {
+            var className = all_contracts[i];
+
+            assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
+          }
+        });
       });
-
     });
-
   });
 
   describe('contracts instances', function() {
@@ -106,25 +113,27 @@ describe('embark.deploy', function() {
     ];
 
     describe('#deploy_contracts', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/instances.yml'
-      });
-      deploy.deploy_contracts("development");
 
       it("should deploy contracts", function() {
-        var all_contracts = ['BarStorage', 'FooStorage'];
-        for(var i=0; i < all_contracts.length; i++) {
-          var className = all_contracts[i];
 
-          assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
-        }
-        assert.notEqual(deploy.deployedContracts.hasOwnProperty('SimpleStorage'), true);
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/instances.yml'
+        })
+        .then(function(deploy) {
+          deploy.deploy_contracts("development");
+
+          var all_contracts = ['BarStorage', 'FooStorage'];
+          for(var i=0; i < all_contracts.length; i++) {
+            var className = all_contracts[i];
+
+            assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
+          }
+          assert.notEqual(deploy.deployedContracts.hasOwnProperty('SimpleStorage'), true);
+        });
       });
-
     });
-
   });
 
   describe('contracts deploy script', function() {
@@ -134,43 +143,53 @@ describe('embark.deploy', function() {
     ];
 
     describe('#deploy_contracts', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/arguments3.yml'
-      });
-      deploy.deploy_contracts("development");
 
       it("should deploy contracts", function() {
-        var all_contracts = ['DataSource', 'MyDataSource', 'Manager'];
-        for(var i=0; i < all_contracts.length; i++) {
-          var className = all_contracts[i];
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/arguments3.yml'
+        })
+        .then(function(deploy) {
+          deploy.deploy_contracts("development");
 
-          assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
-        }
+          var all_contracts = ['DataSource', 'MyDataSource', 'Manager'];
+          for(var i=0; i < all_contracts.length; i++) {
+            var className = all_contracts[i];
+
+            assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
+          }
+        });
       });
 
       it("should execute deploy changes", function() {
-        web3.setProvider(new web3.providers.HttpProvider('http://localhost:8101'));
-        web3.eth.defaultAccount = web3.eth.accounts[0];
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/arguments.yml'
+        })
+        .then(function(deploy) {
+          deploy.deploy_contracts("development");
 
-        data_source_abi     = deploy.contractDB['DataSource'].compiled.info.abiDefinition;
-        data_source_address = deploy.deployedContracts['DataSource'];
-        my_data_source_abi     = deploy.contractDB['MyDataSource'].compiled.info.abiDefinition;
-        my_data_source_address = deploy.deployedContracts['MyDataSource'];
-        manager_abi     = deploy.contractDB['Manager'].compiled.info.abiDefinition;
-        manager_address = deploy.deployedContracts['Manager'];
+          web3.setProvider(new web3.providers.HttpProvider('http://localhost:8101'));
+          web3.eth.defaultAccount = web3.eth.accounts[0];
 
-        DataSource = web3.eth.contract(data_source_abi).at(data_source_address);
-        MyDataSource = web3.eth.contract(my_data_source_abi).at(my_data_source_address);
-        ManagerSource = web3.eth.contract(manager_abi).at(manager_address);
+          data_source_abi     = deploy.contractDB['DataSource'].compiled.info.abiDefinition;
+          data_source_address = deploy.deployedContracts['DataSource'];
+          my_data_source_abi     = deploy.contractDB['MyDataSource'].compiled.info.abiDefinition;
+          my_data_source_address = deploy.deployedContracts['MyDataSource'];
+          manager_abi     = deploy.contractDB['Manager'].compiled.info.abiDefinition;
+          manager_address = deploy.deployedContracts['Manager'];
 
-        assert.equal(DataSource.storeData().toNumber(), 5);
-        assert.equal(Manager.data().toString(), my_data_source_address);
+          DataSource = web3.eth.contract(data_source_abi).at(data_source_address);
+          MyDataSource = web3.eth.contract(my_data_source_abi).at(my_data_source_address);
+          ManagerSource = web3.eth.contract(manager_abi).at(manager_address);
+
+          assert.equal(DataSource.storeData().toNumber(), 5);
+          assert.equal(Manager.data().toString(), my_data_source_address);
+        });
       });
-
     });
-
   });
 
   describe('contracts with addresses defined', function() {
@@ -179,28 +198,28 @@ describe('embark.deploy', function() {
     ];
 
     describe('#deploy_contracts', function() {
-      var deploy = setDeployConfig({
-        files: files,
-        blockchain: 'test/support/blockchain.yml',
-        contracts: 'test/support/address.yml'
-      });
-      deploy.deploy_contracts("development");
 
       it("should not deploy contracts with addresses defined", function() {
-        var expected_deploys = ['SimpleStorage', 'BarStorage', 'FooStorage'];
+        setDeployConfig({
+          files: files,
+          blockchain: 'test/support/blockchain.yml',
+          contracts: 'test/support/addresses.yml'
+        })
+        .then(function(deploy) {
 
-        for(var i=0; i < expected_deploys.length; i++) {
-          var className = expected_deploys[i];
+          deploy.deploy_contracts("development");
+          var expected_deploys = ['SimpleStorage', 'BarStorage', 'FooStorage'];
 
-          assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
-        }
+          for(var i=0; i < expected_deploys.length; i++) {
+            var className = expected_deploys[i];
 
-        assert.equal(deploy.deployedContracts['SimpleStorage'], '0x123');
-        assert.equal(deploy.deployedContracts['BarStorage'], '0x234');
+            assert.equal(deploy.deployedContracts.hasOwnProperty(className), true);
+          }
+
+          assert.equal(deploy.deployedContracts['SimpleStorage'], '0x123');
+          assert.equal(deploy.deployedContracts['BarStorage'], '0x234');
+        });
       });
-
     });
-
   });
-
 });
