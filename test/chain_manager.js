6e8871bc81a813fdb711e4ec085653fc09b1eb90
Dan Finlay <danfinlay@apple.com>
2015-12-29 14:33:13 -0800

    remove logs

--- test/chain_manager.js
+++ test/chain_manager.js
@@ -23,7 +23,6 @@ describe('embark.chain_manager', function() {
         chainManager.init('development', blockchainConfig, web3)
         .then(function() {
           var chain = chainManager.chainManagerConfig['0xcd9c11da1e46f86ce40a38b6ef84cfdfa6ea92598a27538f0e87da6d7a5c73d5']
-          console.log(JSON.stringify(chainManager.chainManagerConfig,null,2));
           assert.notEqual(chain, undefined);
         })
         .catch(function(error) {
@@ -87,7 +86,6 @@ describe('embark.chain_manager', function() {
         var chainFile = './test/support/chain_manager.json';
         var content = fs.readFileSync(chainFile).toString();
         var json = JSON.parse(content);
-        console.log(JSON.stringify(json,null,2));
         assert.ok("0x70fe139444c1b6000184d663715ab48d51f04fe5bf2db3260d982ca38c30806f8669c995141cdebe8e4411afe6b1a91024b0b6fc7561ca16d7c187507f75fcae" in json);
         assert.strictEqual(json["0x70fe139444c1b6000184d663715ab48d51f04fe5bf2db3260d982ca38c30806f8669c995141cdebe8e4411afe6b1a91024b0b6fc7561ca16d7c187507f75fcae"].contracts, {"d5d91a8825c5c253dff531ddda2354c4014f5699b7bcbea70207cfdcb37b6c8b":{"name":"Foo","address":"0x123"}});
       });
